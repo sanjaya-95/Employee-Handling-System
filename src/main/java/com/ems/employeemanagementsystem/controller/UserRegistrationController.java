be09ad186367a5f2eb4513d6eb1f0d2385712b4e
cb-tennakoon <exprwebdev@gmail.com>
2024-08-15 23:14:38 +0530

    partially completed UserRegistration,login, registration

--- src/main/java/com/ems/employeemanagementsystem/controller/UserRegistrationController.java
+++ src/main/java/com/ems/employeemanagementsystem/controller/UserRegistrationController.java
@@ -1,4 +1,14 @@
 package com.ems.employeemanagementsystem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registration")
 public class UserRegistrationController {
+    @GetMapping
+    public String showRegistrationForm(){
+        return "registration";
+    }
 }
