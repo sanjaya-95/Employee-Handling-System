0275b6bf5e52bed81fc3303307fa8e26a8175e02
cb-tennakoon <exprwebdev@gmail.com>
2024-08-15 06:54:16 +0530

    partially completed index,login,registration pages

--- src/main/java/com/ems/employeemanagementsystem/controller/MainController.java
+++ src/main/java/com/ems/employeemanagementsystem/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.ems.employeemanagementsystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/login")
     public String login(){
+
         return "login";
     }
+    @GetMapping("/")
+    public String mainPage(){
+        return "index";
+    }
+
+    @GetMapping("/hello-world")
+    public String helloWorld(Model model){
+        model.addAttribute("message", "Hello about!");
+        return "about";
+    }
+
 }
